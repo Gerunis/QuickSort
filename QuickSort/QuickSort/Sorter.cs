dfec5d03819f9fa844adcd4f2eaa51b57575bee1
Gerunis <Gerunis@users.noreply.github.com>
2019-03-12 15:20:06 +0500

    1.0

---
+++ QuickSort/QuickSort/Sorter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public class Sorter
+    {
+        public static void QuickSort(int[] array)
+        {
+            if (array is null) return;
+            Sort(array, 0, array.Length - 1);
+        }
+
+        static void Sort(int[] array, int start, int end)
+        {
+            if(end > start)
+            {
+                var p = Divide(array, start, end);
+                Sort(array, start, p - 1);
+                Sort(array, p + 1, end);
+            }
+        }
+
+        static int Divide(int[] array, int start, int end)
+        {
+            var pivot = array[(start + end) / 2];
+            var i = start;
+            var j = end;
+
+            while (true)
+            {
+                for (; array[i] < pivot; i++) ;
+                for (; array[j] > pivot; j--) ;
+
+                if (i >= j) return j;
+                if (array[i] == array[j]) i++;
+                else
+                {
+                    var e = array[i];
+                    array[i] = array[j];
+                    array[j] = e;
+                }
+            }
+        }
+    }
+}
---
+++ QuickSort/QuickSort/SorterTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace QuickSort
+{
+    [TestFixture]
+    class SorterTests
+    {
+        public void CheckSort(int[] array)
+        {
+            for (var i = 0; i < array.Length - 1; i++)
+                Assert.True(array[i] <= array[i + 1]);
+        }
+
+
+
+        [Test]
+        public void SortedArray()
+        {
+            var array = new int[] { 1, 2, 3 };
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void TwoArray()
+        {
+            var array = new int[] { 2, 2, 2 };
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void ShortArray()
+        {
+            var array = new int[] { 1 };
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void SimpleArray()
+        {
+            var array = new int[] { 2, 1 };
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void OneNumberArray()
+        {
+            var array = new int[10];
+            Sorter.QuickSort(array);
+            CheckSort(array);
+        }
+
+        [Test]
+        public void RandomArray()
+        {
+            var rnd = new Random();
+            var array = new int[1000];
+            for (var i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(-100, 100);
+            Sorter.QuickSort(array);
+            for(var i = 0; i < 10; i++)
+            {
+                var j = rnd.Next(0, array.Length - 1);
+                Assert.GreaterOrEqual(array[j + 1], array[j]);
+            }
+        }
+
+        [Test]
+        public void EmptyArray()
+        {
+            Sorter.QuickSort(null);
+        }
+
+        [Test]
+        public void BigRandomArray()
+        {
+            var rnd = new Random();
+            var array = new int[1500000000];
+            for (var i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(-100, 100);
+            Sorter.QuickSort(array);
+            for (var i = 0; i < 1000; i++)
+            {
+                var j = rnd.Next(0, array.Length - 1);
+                Assert.GreaterOrEqual(array[j + 1], array[j]);
+            }
+        }
+    }
+}
