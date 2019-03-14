7d5619c4bff9dac5fe01c8ecdaf8ed2ab61fafab
Gerunis <Gerunis@users.noreply.github.com>
2019-03-14 23:29:38 +0500

    Done

--- QuickSort/QuickSort/Program.cs
+++ QuickSort/QuickSort/Program.cs
@@ -10,16 +10,7 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
-            var rnd = new Random();
-            var array = new int[5];
-            for (var i = 0; i < array.Length; i++)
-                array[i] = rnd.Next(0, 10);
-            Sorter.QuickSort(array);
-            for (var i = 0; i < 1000; i++)
-            {
-                var j = rnd.Next(0, array.Length - 1);
-                Console.Write(array[j + 1] <= array[j]);
-            }
+            
         }
     }
 }
